877ae30c7620a1e06e16bf85761eb8cda16e0945
McQuinTrix <harshal.carpenter@gmail.com>
2025-09-28 13:25:15 -0700

    chore: end of first vdeo

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next/types"
+
+export const metadata: Metadata = {
+    title: 'Home'
+}
+
 export default function Home() {
     return <h1>Home Page</h1>
 }
--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next/types"
+
+export const metadata: Metadata = {
+    title: 'Tickets'
+}
+
 export default function Tickets() {
     return <h1>Tickets Page</h1>
 }
